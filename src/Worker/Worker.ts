7ff1ee83c1fe8d181a31d824b16dee4d2072b040
Siddhant Misra <flaxeneel2@gmail.com>
2022-03-16 23:35:18 +0100

    ipc basics

--- src/Worker/Worker.ts
+++ src/Worker/Worker.ts
@@ -1,12 +1,20 @@
 import EventEmitter from "events";
+import Task from "../Task/Task";
+import {ChildProcess,fork} from "child_process";
 export default class Worker extends EventEmitter {
     private queuedTasks: number = 0
+    private childProcess: ChildProcess
     constructor() {
         super();
-
+        this.childProcess = fork("./Thread.js")
     }
 
     getQueueLength() {
         return this.queuedTasks
     }
+
+    execTask(task: Task) {
+        this.childProcess.send({task: task})
+    }
 }
+
